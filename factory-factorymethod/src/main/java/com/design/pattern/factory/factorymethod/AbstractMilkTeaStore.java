357a6bff7ac043255aecfdec1067a7fa9c4bea4f
zhuzhenke <zhenke.zhu@lazada.com>
2019-04-26 09:31:56 +0800

    factorymethod pattern ： add introduce

--- factory-factorymethod/src/main/java/com/design/pattern/factory/factorymethod/AbstractMilkTeaStore.java
+++ factory-factorymethod/src/main/java/com/design/pattern/factory/factorymethod/AbstractMilkTeaStore.java
@@ -1,9 +1,11 @@
 package com.design.pattern.factory.factorymethod;
 
 
-
 /**
  * Expanding Stores: Multi-Store Model
+ * 工厂方法让类将实例化推迟到子类
+ * <p>
+ * 一般也可以提供一个默认的工厂方法，这么一来当没有子类时，可以使用默认的工厂方法来创建产品
  *
  * @author zhuzhenke
  * @date 2019/4/21
@@ -11,6 +13,9 @@ package com.design.pattern.factory.factorymethod;
 public abstract class AbstractMilkTeaStore {
 
     public void buyMilkTea(String type) {
+        /**
+         * 工厂方法模式：通过让子类决定该创建的对象是什么，来达到将对象创建的过程封装的目的。
+         */
         MilkTea milkTea = createMilkTea(type);
 
         milkTea.prepare();
@@ -19,5 +24,10 @@ public abstract class AbstractMilkTeaStore {
         milkTea.delivery();
     }
 
+    /**
+     * 工厂方法是抽象的
+     * 这里工厂方法返回一个产品(奶茶).超类中定义的方法，通常使用工厂方法的返回值
+     * 工厂方法将客户(buyMilkTea方法)和实际创建具体产品的代码分隔开来
+     */
     protected abstract MilkTea createMilkTea(String type);
 }
--- factory-simplefactory/src/main/java/com/design/pattern/factory/simplefactory/SimpleMilkTeaFactory.java
+++ factory-simplefactory/src/main/java/com/design/pattern/factory/simplefactory/SimpleMilkTeaFactory.java
@@ -6,6 +6,10 @@ import com.design.pattern.factory.simplefactory.types.MilkTeaTypeEnum;
 import com.design.pattern.factory.simplefactory.types.RedBeanMilkTea;
 
 /**
+ * 简单工厂其实不是一种设计模式，反而像是一种编码习惯
+ * 简单工厂在这里主要的作用是将创建过程的代码放在一个类中，方便管理。后续需要修改只要在这里改就行了
+ * 同时也是一个解耦功能，将实例化的代码从客户代码中删除。
+ *
  * @author zhuzhenke
  * @date 2019/4/21
  */
